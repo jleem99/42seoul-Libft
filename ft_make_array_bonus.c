0133de33d0a23ba2babfbea27cee4921472f32af
jleem <kch79624@gmail.com>
2021-02-14 15:44:36 +0900

    Fix ft_make_array

--- ft_make_array_bonus.c
+++ ft_make_array_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jleem <jleem@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/09 13:16:26 by jleem             #+#    #+#             */
-/*   Updated: 2021/02/04 01:27:20 by jleem            ###   ########.fr       */
+/*   Updated: 2021/02/14 15:44:25 by jleem            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ t_array	*ft_make_array(size_t size)
 
 	if (!(ret = malloc(sizeof(t_array))))
 		return (NULL);
-	if (!(ret->data = malloc(sizeof(void *) * size)))
+	if (!(ret->data = malloc(sizeof(void *) * size)) && size != 0)
 	{
 		free(ret);
 		return (NULL);
