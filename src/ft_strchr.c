229fcd17437a900b79741fb1d3695e1713566b53
jleem <kch79624@gmail.com>
2021-07-07 05:27:56 +0900

    Fix strchr, strrchr

--- src/ft_strchr.c
+++ src/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: jleem <jleem@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/27 00:09:19 by jleem             #+#    #+#             */
-/*   Updated: 2021/02/04 01:28:43 by jleem            ###   ########.fr       */
+/*   Updated: 2021/07/07 05:27:35 by jleem            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_strchr(char const *s, int c)
 {
 	while (*s)
 	{
-		if (*s == c)
+		if (*s == (char)c)
 			return ((char *)s);
 		s++;
 	}
--- src/ft_strrchr.c
+++ src/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: jleem <jleem@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/27 00:09:29 by jleem             #+#    #+#             */
-/*   Updated: 2021/07/07 05:06:36 by jleem            ###   ########.fr       */
+/*   Updated: 2021/07/07 05:27:01 by jleem            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_strrchr(char const *s, int c)
 		i++;
 	i++;
 	while (i-- != 0)
-		if (s[i] == c)
+		if (s[i] == (char)c)
 			return ((char *)s + i);
 	return (NULL);
 }
